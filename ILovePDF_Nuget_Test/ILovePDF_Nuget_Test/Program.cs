5611ea329be97e709a5cb788789a26b28f1316f7
Mario Luanco <mariomash@gmail.com>
2017-07-24 13:10:58 +0200

    NuGet Package updated to 1.1.0

---
+++ ILovePDF_Nuget_Test/ILovePDF_Nuget_Test/Program.cs
@@ -0,0 +1,34 @@
+﻿using LovePdf.Core;
+using LovePdf.Model.Task;
+using LovePdf.Model.TaskParams;
+
+namespace ILovePDF_Nuget_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var api = new LovePdfApi(@"project_public_3fedaeb8b6ff0e34a849c049422f4725_ZQHs-56404af1d4525a22201eeacc0b8e4ed0", @"secret_key_e6e42ebc47aaa75f7161b307a3464297__tMrk149492f181e7a12e0eae08527cfa166a");
+
+            //create compress task
+            var task = api.CreateTask<CompressTask>();
+
+
+            //file variable contains server file name
+            var file = task.AddFile(@"should-work.pdf");
+
+            var taskParams = new CompressParams()
+            {
+                OutputFileName = @"test"
+            };
+
+            //proces added files
+            //time var will contains information about time spent in process
+            var time = task.Process(taskParams);
+
+            //download files to specific folder
+            task.DownloadFile();
+
+        }
+    }
+}
