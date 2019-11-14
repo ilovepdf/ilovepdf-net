5a31e379688da8d0f4d313bd1ccb48e3bd2d7431
Oleg Rakhmatulin <oleg.rakhmatulin@gmail.com>
2019-11-14 19:39:42 +0100

    Provide SDK version compatible with .NET Framework 4.5 (#20)
    * Issue #19 - convert all projects into new .csproj format for better multi-platform support.
    * Issue #19 - add .NET Framework 4.5 as target and provide an old JWT package for it.
    * Issue #19 - NuGet package `JWT` replaced with package `jose-jwt` (has better .NET 4.5 support).

--- ILovePDF/Tests/Properties/AssemblyInfo.cs
+++ ILovePDF/Tests/Properties/AssemblyInfo.cs
@@ -1,19 +1,6 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("76837053-65fd-43f0-b69d-1c6965b302e2")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
