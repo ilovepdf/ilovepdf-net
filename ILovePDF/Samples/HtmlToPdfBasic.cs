266bc613169d9a3e30e5cfaeb9dc4f3a8c1443bd
Abdurahim Khudoyberdiev <abdurahim.khudoyberdiev@gmail.com>
2022-10-10 22:04:53 +0500

    Issue#32 - Added the HtmlToPdf tool support enhancement

---
+++ ILovePDF/Samples/HtmlToPdfBasic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using LovePdf.Core;
+using LovePdf.Model.Task;
+using LovePdf.Model.TaskParams;
+
+namespace Samples
+{
+    [SuppressMessage("ReSharper", "UnusedVariable")]
+    public class HtmlToPdfBasic
+    {
+        public void DoTask()
+        {
+            var api = new LovePdfApi("PUBLIC_KEY", "SECRET_KEY");
+
+            //create protect task
+            var task = api.CreateTask<HtmlToPdfTask>();
+
+            //file variable contains server file name
+            var file = task.AddFile(new Uri("https://ilovepdf.com"));
+
+            //proces added files
+            //time var will contains information about time spent in process
+            var time = task.Process(new HTMLtoPDFParams());
+            task.DownloadFile("path");
+        }
+    }
+}
