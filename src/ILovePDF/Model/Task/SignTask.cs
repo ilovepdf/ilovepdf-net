14953e11635289ebc0c918050169de6041dafc3b
teamcrombie <equipo.crombie@4rsoluciones.com>
2024-08-05 09:59:41 -0300

    fix set brand logo sign tool

--- src/ILovePDF/Model/Task/SignTask.cs
+++ src/ILovePDF/Model/Task/SignTask.cs
@@ -6,6 +6,10 @@ using System.Collections.Generic;
 using LovePdf.Core.Sign;
 using LovePdf.Model.TaskParams.Sign;
 using System.Globalization;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace LovePdf.Model.Task
 {
@@ -14,6 +18,8 @@ namespace LovePdf.Model.Task
     /// </summary>
     public class SignTask : LovePdfTask
     {
+        private static SignTask _instance;
+        public static SignTask Instance => _instance ?? (_instance = new SignTask());
         /// <inheritdoc />
         public override String ToolName => EnumExtensions.GetEnumDescription(TaskName.Sign);
 
@@ -97,6 +103,57 @@ namespace LovePdf.Model.Task
         public System.Threading.Tasks.Task<ReceiverInfoResponse> SendRemindersAsync(string tokenRequester)
         {
             return RequestHelper.Instance.SendRemindersAsync(ServerUrl, tokenRequester);
+        }
+
+        /// <summary>
+        ///     Upload file to the ILovePdf server from local drive.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="taskId">if no task provided will be used last one from create task method.</param>
+        /// <param name="serverUrl"></param>
+        /// <returns>Server file name</returns>
+        [SuppressMessage("Microsoft.Design", "CA1057:StringUriOverloadsCallSystemUriOverloads")]
+        public UploadTaskResponse AddFileLogo(Uri path, String taskId, Uri serverUrl)
+        {
+            return AddFileLogo(path, taskId, serverUrl, Rotate.Degrees0);
+        }
+
+        /// <summary>
+        ///     Upload file to the ILovePdf server from local drive.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="taskId">if no task provided will be used last one from create task method.</param>
+        /// <param name="serverUrl"></param>
+        /// <param name="rotate"></param>
+        /// <returns>Server file name</returns>
+        [SuppressMessage("Microsoft.Design", "CA1057:StringUriOverloadsCallSystemUriOverloads")]
+        public UploadTaskResponse AddFileLogo(Uri cloudLink, String taskId, Uri serverUrl, Rotate rotate)
+        {
+            if (cloudLink == null)
+                throw new ArgumentException("cannot be null", nameof(cloudLink));
+
+            var requestTaskId = String.IsNullOrWhiteSpace(taskId) ? TaskId : taskId;
+
+            var response = RequestHelper.Instance.UploadFile(serverUrl, cloudLink, requestTaskId);
+
+            //TODO check filename
+            var fileName = Path.GetFileName(cloudLink.AbsoluteUri);
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                var host = cloudLink.Host;
+                if (host.Contains('.'))
+                {
+                    var parts = cloudLink.Host.Split('.');
+                    fileName = parts[parts.Length - 2];
+                }
+                else
+                {
+                    fileName = host;
+                }
+            }
+
+            return response;
         }
 
         #region Unsupported methods 
