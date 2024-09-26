cf1e0e516e543b94de8183a5b019b2727dd31920
teamcrombie <equipo.crombie@4rsoluciones.com>
2024-09-26 09:18:22 -0300

    map pdf pages & pdfpagenumber properties

--- src/ILovePDF/Core/UploadTaskResponse.cs
+++ src/ILovePDF/Core/UploadTaskResponse.cs
@@ -13,5 +13,15 @@ namespace iLovePdf.Core
         /// </summary>
         [JsonProperty("server_filename")]
         public String ServerFileName { get; set; }
+        /// <summary>
+        ///     Server file name
+        /// </summary>
+        [JsonProperty("pdf_pages")]
+        public String[] PdfPages { get; set; }
+        /// <summary>
+        ///     Server file name
+        /// </summary>
+        [JsonProperty("pdf_page_number")]
+        public String PdfPageNumber { get; set; }
     }
 }
